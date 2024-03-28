4718a36d9ae8eff1389bf0eedbf9900faf61e139
martinkelly23 <martkelly92@gmail.com>
2024-03-28 22:33:07 +0100

    test: red getting the records info

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -11,12 +11,24 @@ class TestExtractor:
         extractor = Extractor(file_src=health_file_path)
 
         assert set(extractor.get_record_types()) == {'HKQuantityTypeIdentifierStepCount',
-                                                 'HKQuantityTypeIdentifierHeartRate'}
+                                                     'HKQuantityTypeIdentifierHeartRate'}
 
-    def test_get_values_when_given_record_type(self):
+    def test_get_records_when_given_record_type(self):
         test_dir = os.path.dirname(__file__)
         health_file_path = os.path.join(test_dir, 'resources', 'health_data.xml')
 
         extractor = Extractor(file_src=health_file_path)
 
-        assert extractor.get_record_values("HKQuantityTypeIdentifierStepCount") == ["1500", "2500"]
+        assert extractor.get_records("HKQuantityTypeIdentifierStepCount") == [
+            {
+                "value": "1500",
+                "start_date": "2022-03-25 00:00:00 +0000",
+                "end_date": "2022-03-25 00:00:00 +0000",
+                "creation_date": "2022-03-25 08:00:00 +0000"
+            }, {
+                "value": "2500",
+                "start_date": "2022-03-25 12:00:00 +0000",
+                "end_date": "2022-03-25 12:00:00 +0000",
+                "creation_date": "2022-03-25 08:00:00 +0000"
+            }
+        ]
