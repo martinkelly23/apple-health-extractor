c799388a24bf62dffb6b6337b2d855619f6be777
martinkelly23 <martkelly92@gmail.com>
2024-03-28 22:08:26 +0100

    test: red get values of given record

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -13,3 +13,10 @@ class TestExtractor:
         assert set(extractor.record_types()) == {'HKQuantityTypeIdentifierStepCount',
                                                  'HKQuantityTypeIdentifierHeartRate'}
 
+    def test_get_values_when_given_record_type(self):
+        test_dir = os.path.dirname(__file__)
+        health_file_path = os.path.join(test_dir, 'resources', 'health_data.xml')
+
+        extractor = Extractor(file_src=health_file_path)
+
+        assert extractor.record_values("HKQuantityTypeIdentifierStepCount") == ["1500", "2500"]
