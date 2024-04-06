8220d53bac9aa6c218888f900765ec22e5bf8683
martinkelly23 <martkelly92@gmail.com>
2024-04-06 11:53:37 +0200

    test: red expecting json

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -4,6 +4,15 @@ from extractor.extractor import Extractor
 
 
 class TestExtractor:
+    def test_get_json(self):
+        test_dir = os.path.dirname(__file__)
+        health_file_path = os.path.join(test_dir, 'resources', 'health_data.xml')
+
+        extractor = Extractor(file_src=health_file_path)
+
+        assert extractor.get_json() == {
+
+        }
     def test_extract_record_types(self):
         test_dir = os.path.dirname(__file__)
         health_file_path = os.path.join(test_dir, 'resources', 'health_data.xml')
