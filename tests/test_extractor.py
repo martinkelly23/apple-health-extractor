837ca5f92cd5bd989c37ea373533426eba3c4fb2
martinkelly23 <martkelly92@gmail.com>
2024-04-07 00:58:27 +0200

    test: red expecting content in the csv

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -48,13 +48,26 @@ class TestExtractor:
     def test_save_to_csv(self):
         test_dir = os.path.dirname(__file__)
         health_file_path = os.path.join(test_dir, "resources", "health_data.xml")
+        expected_csv = (
+            "@type,@sourceName,@sourceVersion,@unit,@creationDate,@startDate,@endDate,@value\n"
+            "HKQuantityTypeIdentifierStepCount,iPhone,12.1,count,2022-03-25 08:00:00 "
+            "+0000,2022-03-25 00:00:00 +0000,2022-03-25 00:00:00 +0000,1500\n"
+            "HKQuantityTypeIdentifierStepCount,iPhone,12.1,count,2022-03-25 08:00:00 "
+            "+0000,2022-03-25 12:00:00 +0000,2022-03-25 12:00:00 +0000,2500\n"
+            "HKQuantityTypeIdentifierHeartRate,Apple Watch,8.0,count/min,2022-03-25 "
+            "08:30:00 +0000,2022-03-25 08:30:00 +0000,2022-03-25 08:30:00 +0000,72\n"
+        )
 
         extractor = Extractor(file_src=health_file_path)
 
         extractor.save_to_csv()
+        temp_file_path = "health_data.csv"
+        assert os.path.exists(temp_file_path)
+        with open(temp_file_path, "r") as file:
+            csv_data = file.read()
+            assert csv_data == expected_csv
 
-        assert os.path.exists("health_data.csv")
-        os.remove("health_data.csv")
+        os.remove(temp_file_path)
 
     def test_extract_record_types(self):
         test_dir = os.path.dirname(__file__)
